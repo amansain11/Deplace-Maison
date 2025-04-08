0f7f642a0964daacdf3fc1613c917dbd37f774d7
amansain11 <sainaman1819@gmail.com>
2025-04-08 21:55:04 +0530

    Add sidebar

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './STYLES/style.css';
 import './STYLES/modern-normalize.css';
 import './STYLES/header.css';
-import './STYLES/main.css';
+import './STYLES/main-and-sidebar.css';
 import './STYLES/hero-section.css';
 import './STYLES/i-section.css';
 import './STYLES/morquee-section.css';
