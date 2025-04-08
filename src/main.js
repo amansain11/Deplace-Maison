cf7a6475a2189a9c6c3bed4ae5f12e85ef013d94
amansain11 <sainaman1819@gmail.com>
2025-04-08 17:08:11 +0530

    Add footer

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import './STYLES/morquee-section.css';
 import './STYLES/e-section.css';
 import './STYLES/slider-section.css';
 import './STYLES/quote-section.css';
+import './STYLES/footer.css';
 
 import { slider } from './img-slider.js';
 
